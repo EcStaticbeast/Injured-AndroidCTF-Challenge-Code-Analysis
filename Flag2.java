5a835d6881c37b10628e5b9115396939b99be2eb
EcStaticbeast <130395352+EcStaticbeast@users.noreply.github.com>
2023-11-28 12:23:33 +0530

    Update Flag2.java

--- Flag2.java
+++ Flag2.java
@@ -14,14 +14,14 @@ public class FlagTwoActivity extends androidx.appcompat.app.c {        //declari
         int i = this.w;        // initializing integer i with the current interface with variable w
         if (i == 0) {        // if conditional block,if the value of integer i is 0 then execute if block
             Snackbar X = Snackbar.X(view, "Key words Activity and exported.", 0);    // creates a snackbar, initiates the snackbar with the message
-            X.Y("Action", null);        //
-            X.N();
-            this.w++;
-        } else if (i == 1) {
-            Snackbar X2 = Snackbar.X(view, "Exported Activities can be accessed with adb or Drozer.", 0);
-            X2.Y("Action", null);
-            X2.N();
-            this.w = 0;
+            X.Y("Action", null);        // declares the snapbar with the method action
+            X.N();        //declares method N() to display the message from snackbar
+            this.w++;        //in the current instance value of variable w is incremented
+        } else if (i == 1) {    // else condition, if the value of integer i is equal to 1 then 
+            Snackbar X2 = Snackbar.X(view, "Exported Activities can be accessed with adb or Drozer.", 0);    //creates snackbar x2, initiates the snackbar with the message
+            X2.Y("Action", null);        //adds an action "Action" to the snackbar with a null callback
+            X2.N();        //declares N() method to display snackbar
+            this.w = 0;        //in the current interface,value of variable w is set to 0
         }
     }
 
